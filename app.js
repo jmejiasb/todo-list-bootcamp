e9ddd774ecd0cbc1f1c3cb6305c509c2c59450ce
Jose Mejias <jmmejias11@gmail.com>
2022-09-05 20:24:44 -0300

    initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+var items = [];
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+
+  var today = new Date();
+  var currentDay = today.getDay();
+  var options ={
+    weekday: "long",
+    day:"numeric",
+    month: "long"
+  };
+
+  var day = today.toLocaleDateString("en-EN", options);
+
+  res.render("list", {day: day, items: items});
+});
+
+app.post("/", function(req, res){
+  let item = req.body.newItem;
+
+  items.push(item);
+
+  res.redirect("/");
+
+});
+
+app.listen(3000, function() {
+  console.log("Server up in port 3000");
+});
