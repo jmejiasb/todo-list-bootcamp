6fa0cff326ca7f5a5bda6c0b1ad182d97e27111c
Jose Mejias <jmmejias11@gmail.com>
2022-11-22 07:29:13 -0300

    Add custom URL handling

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB")
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
 
 const itemsSchema = {
   name: String
@@ -19,39 +19,87 @@ const itemsSchema = {
 
 const Item = mongoose.model("Item", itemsSchema);
 
+const listsSchema = {
+  name: String,
+  items: [itemsSchema]
+};
+
+const List = mongoose.model("List", listsSchema);
+
 const item1 = new Item({
   name: "Welcome to the To Do List!"
 });
 
-const defaultItems = [item1]
+const item2 = new Item({
+  name: "Have a great day!"
+});
+
+
+const defaultItems = [item1, item2];
 
 app.get("/", function(req, res) {
+
   Item.find({}, function(err, foundItems){
-    if(foundItems.lenght === 0){
+    console.log(foundItems);
+    if (foundItems.lenght === 0){
       Item.insertMany(defaultItems, function(err){
         if (err){
           console.log(err);
         } else {
           console.log("Successfully added default Items");
-        };
+        }
       });
       res.redirect("/");
     } else {
       res.render("list", {listTitle: "To Do List", items: foundItems});
-    };
+    }
   });
 });
 
 app.post("/", function(req, res){
   const itemName = req.body.newItem;
+  const listName = req.body.list
 
   const item = new Item({name: itemName});
 
-  item.save();
+  if (listName === "To Do List"){
+    item.save();
+    res.redirect("/");
+  } else {
+    List.findOne({name: listName}, function(err, foundList){
+      foundList.items.push(item);
+      foundList.save();
+      res.redirect("/" + listName);
+    });
+  }
 
-  res.redirect("/");
 
 });
+
+app.get("/list/:customList", function(req, res){
+
+  const customListName = req.params.customList;
+
+  List.findOne({name: customListName}, function(err, foundList){
+    if (!err){
+      if(!foundList){
+        // Create a new List
+        const list = new List({
+          name: customListName,
+          items: defaultItems
+        });
+
+        list.save();
+        res.redirect("/" + customListName);
+      } else {
+        //Show an existing list
+        res.render("list", {listTitle: foundList.name, items: foundList.items});
+      }
+    }
+  });
+
+});
+
 
 app.post("/delete", function(req, res){
 
